efe949f0dec3bfafc5219fc9fdcbb0f8ef770ff6
Auios <LoneAuios@gmail.com>
2023-01-28 10:44:29 -0500

    CircleFilled + CircleFilledOutline

--- BadLib/BadLib/Gfx.cs
+++ BadLib/BadLib/Gfx.cs
@@ -3,10 +3,8 @@ using System.Numerics;
 
 namespace BadLib;
 
-public static class Gfx
-{
-    public static void Line(Vector2 start, Vector2 end, Color color, float lineSize = 1)
-    {
+public static class Gfx {
+    public static void Line(Vector2 start, Vector2 end, Color color, float lineSize = 1) {
         RlGl.rlSetTexture(RlGl.rlGetTextureIdDefault());
         RlGl.rlSetLineWidth(lineSize);
         RlGl.rlColor4ub(color.r, color.g, color.b, color.a);
@@ -19,8 +17,7 @@ public static class Gfx
         RlGl.rlEnd();
     }
 
-    public static void RectangleLine(Vector2 position, Vector2 size, Color color, float lineSize = 1)
-    {
+    public static void RectangleLine(Vector2 position, Vector2 size, Color color, float lineSize = 1) {
         RlGl.rlSetTexture(RlGl.rlGetTextureIdDefault());
         RlGl.rlSetLineWidth(lineSize);
         RlGl.rlColor4ub(color.r, color.g, color.b, color.a);
@@ -48,8 +45,7 @@ public static class Gfx
 
     public static void RectangleLine(Rectangle rectangle, Color color, float lineSize = 1) => RectangleLine(new Vector2(rectangle.X, rectangle.Y), new Vector2(rectangle.width, rectangle.height), color, lineSize);
 
-    public static void Circle(Vector2 position, float radius, Color color, int segments = 32, float lineSize = 1)
-    {
+    public static void Circle(Vector2 position, float radius, Color color, int segments = 32, float lineSize = 1) {
         if (radius <= 0.0f)
             radius = 0.01f;
 
@@ -61,13 +57,37 @@ public static class Gfx
 
         RlGl.rlBegin(RlGl.RL_LINES);
         {
-            for (int i = 0; i < 360; i += stepSize)
-            {
+            for (int i = 0; i < 360; i += stepSize) {
                 RlGl.rlVertex2f(position.X + (float)Math.Sin(RayMath.DEG2RAD * i) * radius, position.Y + (float)Math.Cos(RayMath.DEG2RAD * i) * radius);
                 RlGl.rlVertex2f(position.X + (float)Math.Sin(RayMath.DEG2RAD * (i + stepSize)) * radius, position.Y + (float)Math.Cos(RayMath.DEG2RAD * (i + stepSize)) * radius);
             }
         }
         RlGl.rlEnd();
+    }
+
+    public static void CircleFilled(Vector2 position, float radius, Color color, int segments = 32) {
+        if (radius <= 0.0f)
+            radius = 0.01f;
+
+        RlGl.rlSetTexture(RlGl.rlGetTextureIdDefault());
+        RlGl.rlColor4ub(color.r, color.g, color.b, color.a);
+
+        int stepSize = 360 / segments;
+
+        RlGl.rlBegin(RlGl.RL_TRIANGLES);
+        {
+            for (int i = 0; i < 360; i += stepSize) {
+                RlGl.rlVertex2f(position.X, position.Y);
+                RlGl.rlVertex2f(position.X + (float)Math.Sin(RayMath.DEG2RAD * i) * radius, position.Y + (float)Math.Cos(RayMath.DEG2RAD * i) * radius);
+                RlGl.rlVertex2f(position.X + (float)Math.Sin(RayMath.DEG2RAD * (i + stepSize)) * radius, position.Y + (float)Math.Cos(RayMath.DEG2RAD * (i + stepSize)) * radius);
+            }
+        }
+        RlGl.rlEnd();
+    }
+
+    public static void CircleFilledOutline(Vector2 position, float radius, Color color, Color outlineColor, int segments = 32, float lineSize = 1) {
+        CircleFilled(position, radius, color, segments);
+        Circle(position, radius, outlineColor, segments, lineSize);
     }
 
     public static void Texture(Texture texture, Vector2 position, float scale, Color color) => Raylib.DrawTextureEx(texture, position, 0, scale, color);
